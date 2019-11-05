2f5dd010a2e94e0340d7a24156ac7f7fa78fc22c
vjspranav <pranavasri@live.in>
2019-11-06 01:14:04 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vjspranav/Telegrambot-go
+
+go 1.13
+
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4 // indirect
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "os"
+    tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+    var (
+        port      = os.Getenv("PORT")       // sets automatically
+        publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+        token     = os.Getenv("TOKEN")      // you must add it to your config vars
+    )
+
+    webhook := &tb.Webhook{
+        Listen:   ":" + port,
+        Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+    }
+    
+    pref := tb.Settings{
+        Token:  token,
+        Poller: webhook,
+    }
+
+    b, err := tb.NewBot(pref)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    b.Handle("/hello", func(m *tb.Message) {
+    b.Send(m.Sender, "Hi!")
+    })
+}
+
